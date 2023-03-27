d20d8ad5c92a5c8c39841aa3ec6f6089cf45d211
Chaitanya Sawant <chaitanyasawant05072001@gmail.com>
2023-03-27 12:59:32 +0530

    populated author

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -436,7 +436,7 @@ exports.searchBlogs = catchAsync(async (req, res, next) => {
     ...[{ createdAt: { $gte: firstDay } }, { createdAt: { $lt: lastDay } }]
   );
   const [data, total] = await Promise.all([
-    contentModel.find(filter),
+    contentModel.find(filter).populate("author"),
     (await contentModel.find(filter)).length,
   ]);
 
