aa69cf16ae5f85225a8efabe8b80d731af1f4dc4
Ajmalazeem <100754639+dev-ideausher@users.noreply.github.com>
2023-06-16 12:11:25 +0530

    limit changed

--- app.js
+++ app.js
@@ -30,8 +30,16 @@ app.use(
     ],
   })
 );
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// Express 4.0
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
+
+// // Express 3.0
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ limit: '10mb' }));
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
